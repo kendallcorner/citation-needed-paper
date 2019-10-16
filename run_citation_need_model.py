5a4ed2b715c5db7555a2265f50990def74cc7369
Kendall Werts <kmwerts@gmail.com>
2019-10-16 10:55:44 -0500

    removed linting to it's easier to compare to the original

--- run_citation_need_model.py
+++ run_citation_need_model.py
@@ -9,12 +9,12 @@ from bs4 import BeautifulSoup
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import confusion_matrix
 
 from keras.utils import to_categorical
 
 from keras import backend as K
-K.set_session(K.tf.Session(config=K.tf.ConfigProto(
-    intra_op_parallelism_threads=10, inter_op_parallelism_threads=10)))
+K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=10, inter_op_parallelism_threads=10)))
 
 '''
     Set up the arguments and parse them.
@@ -23,24 +23,13 @@ K.set_session(K.tf.Session(config=K.tf.ConfigProto(
 
 def get_arguments():
     parser = argparse.ArgumentParser(
-        description='Use this script to determinee whether a statement needs a\
-        citation or not.')
-    parser.add_argument('-i', '--input', help='The input file from which we\
-        read the statements. Lines contains tab-separated values: the\
-        statement, the section header, and additionally the binary label\
-        corresponding to whether the sentence has a citation or not in the\
-        original text. This can be set to 0 if no evaluation is needed.\
-        ', required=True)
-    parser.add_argument('-o', '--out_dir', help='The output directory where we\
-        store the results', required=True)
-    parser.add_argument('-m', '--model', help='The path to the model which we\
-        use for classifying the statements.', required=True)
-    parser.add_argument('-v', '--vocab', help='The path to the vocabulary of\
-        words we use to represent the statements.', required=True)
-    parser.add_argument('-s', '--sections', help='The path to the vocabulary\
-        of section with which we trained our model.', required=True)
-    parser.add_argument('-l', '--lang', help='The language that we are parsing\
-        now.', required=False, default='en')
+        description='Use this script to determinee whether a statement needs a citation or not.')
+    parser.add_argument('-i', '--input', help='The input file from which we read the statements. Lines contains tab-separated values: the statement, the section header, and additionally the binary label corresponding to whether the sentence has a citation or not in the original text. This can be set to 0 if no evaluation is needed.', required=True)
+    parser.add_argument('-o', '--out_dir', help='The output directory where we store the results', required=True)
+    parser.add_argument('-m', '--model', help='The path to the model which we use for classifying the statements.', required=True)
+    parser.add_argument('-v', '--vocab', help='The path to the vocabulary of words we use to represent the statements.', required=True)
+    parser.add_argument('-s', '--sections', help='The path to the vocabulary of section with which we trained our model.', required=True)
+    parser.add_argument('-l', '--lang', help='The language that we are parsing now.', required=False, default='en')
 
     return parser.parse_args()
 
@@ -105,24 +94,20 @@ def text_to_word_list(text):
 '''
 
 
-def construct_instance_reasons(statement_path, section_dict_path,
-                               vocab_w2v_path, max_len=-1):
+def construct_instance_reasons(statement_path, section_dict_path, vocab_w2v_path, max_len=-1):
     # Load the vocabulary
     vocab_w2v = pickle.load(open(vocab_w2v_path, 'rb'), encoding='latin1')
 
     # load the section dictionary.
-    section_dict = pickle.load(open(section_dict_path, 'rb'),
-                               encoding='latin1')
+    section_dict = pickle.load(open(section_dict_path, 'rb'), encoding='latin1')
 
-    # Load the statements, the first column is the statement and the second is
-    # the label (True or False)
-    statements = pd.read_csv(statement_path, sep='\t', index_col=None,
-                             error_bad_lines=False, warn_bad_lines=False)
+    # Load the statements, the first column is the statement and the second is the label (True or False)
+    statements = pd.read_csv(statement_path, sep='\t', index_col=None, error_bad_lines=False, warn_bad_lines=False)
 
     # construct the training data
     X = []
     sections = []
-    outstring = []
+    outstring=[]
     for index, row in statements.iterrows():
         try:
             statement_text = text_to_word_list(row['statement'])
@@ -136,19 +121,15 @@ def construct_instance_reasons(statement_path, section_dict_path,
                 else:
                     X_inst.append(vocab_w2v[word])
 
-            # extract the section, and in case the section does not exist in
-            # the model, then assign UNK
+            # extract the section, and in case the section does not exist in the model, then assign UNK
             section = row['section'].strip().lower()
-            sections.append(
-                np.array(
-                    [section_dict[section] if section in section_dict else 0]))
+            sections.append(np.array([section_dict[section] if section in section_dict else 0]))
 
-            # some of the rows are corrupt, thus, we need to check if the\
-            # labels are actually boolean.
+            # some of the rows are corrupt, thus, we need to check if the labels are actually boolean.
 
             X.append(X_inst)
             outstring.append(str(row["statement"]))
-            # entity_id  revision_id timestamp   entity_title    section_id  section prg_idx sentence_idx    statement   citations
+            #entity_id  revision_id timestamp   entity_title    section_id  section prg_idx sentence_idx    statement   citations
 
         except Exception as e:
             print(row)
@@ -223,8 +204,7 @@ if __name__ == '__main__':
     # classify the data
     pred = model.predict([X, sections])
 
-    # store the predictions: printing out the sentence text, the prediction
-    # score, and original citation label.
+    # store the predictions: printing out the sentence text, the prediction score, and original citation label.
     outstr = 'Text\tPrediction\n'
 
     ordered_list = []
